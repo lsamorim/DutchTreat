b4c12cf91a06e0d06f68c895a76e8beae12febe5
lsamorim <lsamorim.it@gmail.com>
2019-02-20 13:09:29 -0300

    Implement login by using Identity Sign Manager

--- src/DutchTreat/Controllers/AccountController.cs
+++ src/DutchTreat/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DutchTreat.Data.Entities;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,10 +14,12 @@ namespace DutchTreat.Controllers
     public class AccountController : Controller
     {
         private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<StoreUser> _signInManager;
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(ILogger<AccountController> logger, SignInManager<StoreUser> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
         }
 
         public IActionResult Login()
@@ -26,5 +31,33 @@ namespace DutchTreat.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginViewModel.Username,
+                                    loginViewModel.Password, loginViewModel.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    if(Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                        return Redirect(Request.Query["ReturnUrl"].First());
+                    }
+                    else
+                    {
+                        return RedirectToAction("Shop", "App");
+                    }
+                }
+            }
+
+            ModelState.AddModelError("", "Failed to login");
+
+            return View();
+        }
+
+
     }
 }
