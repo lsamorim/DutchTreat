617628067ffe5849e30d3b28dca515212807d794
lsamorim <lsamorim.it@gmail.com>
2019-02-20 08:34:49 -0300

    Add products api controller

---
+++ src/DutchTreat/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using DutchTreat.Data;
+using DutchTreat.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace DutchTreat.Controllers
+{
+    [Route("api/[Controller]")]
+    //[ApiController]
+    //[Produces("application/json")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IDutchRepository _repository;
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(IDutchRepository repository, ILogger<ProductsController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        //[ProducesResponseType(200)]
+        //[ProducesResponseType(500)]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            try
+            {
+                return Ok(_repository.GetAllProducts());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something wrong happen: {ex}");
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
